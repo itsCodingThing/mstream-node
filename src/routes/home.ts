3a469d5b45196c79b469446c71cebcc93b4192f2
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2020-11-14 18:28:26 +0530

    adding proper error handling in routes

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -5,28 +5,50 @@ import mongoose from "mongoose";
 const router = express.Router();
 
 router.get("/", async (_req: Request, res: Response) => {
-  const { gfs } = await connectToDb();
-  const docs = await gfs.find().toArray();
-  res.json(docs);
+  try {
+    const { gfs } = await connectToDb();
+    const docs = await gfs.find().toArray();
+
+    res.json({ ok: true, response: docs });
+  } catch (error) {
+    res.json({ ok: false, error: "something went wrong with the backend" });
+  }
 });
 
-router.post("/:songName", async (req: Request, res: Response) => {
-  const { gfs } = await connectToDb();
+router.post("/upload/:songName", async (req: Request, res: Response) => {
+  try {
+    const { gfs } = await connectToDb();
+    const name = req.params.songName;
+    const uploadStream = gfs.openUploadStream(name);
+    req.pipe(uploadStream);
 
-  const name = req.params.songName;
+    uploadStream.on("finish", () => {
+      res.json({ ok: true, songId: uploadStream.id });
+    });
 
-  const uploadStream = gfs.openUploadStream(name);
-  req.pipe(uploadStream);
-  res.json({ ok: true });
+    uploadStream.on("error", () => {
+      res.json({ ok: false, error: "something went wrong with the database" });
+    });
+  } catch (error) {
+    res.json({ ok: false, error: "something went wrong with the backend" });
+  }
 });
 
-router.get("/:id", async (req: Request, res: Response) => {
-  const { gfs } = await connectToDb();
+router.get("/stream/:id", async (req: Request, res: Response) => {
+  try {
+    const { gfs } = await connectToDb();
 
-  const songID = req.params.id;
+    const songID = req.params.id;
+    const id = mongoose.Types.ObjectId(songID);
+    const downloadStream = gfs.openDownloadStream(id);
+    downloadStream.pipe(res);
 
-  const id = mongoose.Types.ObjectId(songID);
-  gfs.openDownloadStream(id).pipe(res);
+    downloadStream.on("error", (err) => {
+      res.json({ ok: false, error: err.message });
+    });
+  } catch (error) {
+    res.json({ ok: false, error: "something went wrong with backend" });
+  }
 });
 
 export default router;
