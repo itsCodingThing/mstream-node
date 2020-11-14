300c1f8fa4669c91bee8ed4c97c4e4babf521e4b
bhanu pratap singh <bhanuparitapsingh09@gmail.com>
2020-11-14 18:58:03 +0530

    adding cors

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import homeRoute from "./routes/home";
 
 const app = express();
 
+app.use(cors());
 app.use("/", homeRoute);
 
 export default app;
