2d1bf05027a0b9d29a446cd50505232cc019d5f1
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-01-07 16:35:27 +0530

    removing stream route from file

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -1,10 +1,7 @@
-import express, { Request, Response } from "express";
-import mongoose from "mongoose";
-import { createWriteStream, readFileSync } from "fs";
-import { join } from "path";
-import { tempDir, gridfs } from "../server";
+import { Request, Response, Router } from "express";
+import { gridfs } from "../server";
 
-const router = express.Router();
+const router = Router();
 
 router.get("/", async (_req: Request, res: Response) => {
     try {
@@ -31,28 +28,6 @@ router.post("/upload/:songName", async (req: Request, res: Response) => {
         });
     } catch (error) {
         res.json({ ok: false, error: "something went wrong with the backend" });
-    }
-});
-
-router.get("/stream/:id", async (req: Request, res: Response) => {
-    try {
-        const songID = req.params.id;
-        const id = mongoose.Types.ObjectId(songID);
-        const downloadStream = gridfs.openDownloadStream(id);
-
-        const writeFile = createWriteStream(join(tempDir, `${id}.mp3`));
-
-        downloadStream.pipe(writeFile);
-
-        downloadStream.on("end", () => {
-            res.send(readFileSync(join(tempDir, `${id}.mp3`)));
-        });
-
-        downloadStream.on("error", (err) => {
-            res.json({ ok: false, error: err.message });
-        });
-    } catch (error) {
-        res.json({ ok: false, error: "something went wrong with backend" });
     }
 });
 
