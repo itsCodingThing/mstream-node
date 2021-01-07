2dcbdc33157f88f1adab40cb5bd9128e4c2cf333
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-01-07 16:36:12 +0530

    setting up the new TempDir and db method

--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,14 @@
 import app from "./app";
 import { connectToDb } from "./database/db";
-import { mkdtempSync } from "fs";
-import { join, sep } from "path";
-import { tmpdir } from "os";
+import TempDir from "./utils/tempDir";
 
 const PORT: number | string = process.env.PORT || 1729;
 
 // Setting a temp directory in system temp directory
-const systemTempDir = tmpdir();
-export const tempDir = mkdtempSync(join(`${systemTempDir}${sep}mstream--`));
+export const tempDir = new TempDir("mstream");
 
 export const gridfs = await connectToDb();
 
 app.listen(PORT, async () => {
-    try {
-        console.log(
-            `${process.env.NODE_ENV === "development" ? process.env.NODE_ENV : ""} server is running on port ${PORT}`,
-        );
-    } catch (error) {
-        console.log(error);
-    }
+    console.log(`server is running on port ${PORT}`);
 });
