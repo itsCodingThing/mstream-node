4ef8e02b6cf77e885aa92933ae9eced88457fbd0
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-01-07 16:34:24 +0530

    changing database setup method

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,22 +1,17 @@
 import { GridFSBucket } from "mongodb";
 import mongoose from "mongoose";
 
-// eslint-disable-next-line prettier/prettier
 const localDb = process.env.DB_URI;
 
 export async function connectToDb(): Promise<GridFSBucket> {
-    try {
-        const connection = await mongoose.createConnection(localDb, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false,
-            useUnifiedTopology: true,
-        });
+    const connection = await mongoose.createConnection(localDb, {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+    });
 
-        return new mongoose.mongo.GridFSBucket(connection.db, {
-            bucketName: "songs",
-        });
-    } catch (error) {
-        console.log(error);
-    }
+    return new mongoose.mongo.GridFSBucket(connection.db, {
+        bucketName: "songs",
+    });
 }
