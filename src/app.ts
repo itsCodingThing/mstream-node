e4ec8c45a930690cf19de7ccd8f1690b18d00497
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-01-07 16:34:47 +0530

    adding new stream route to router

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import cors from "cors";
 import homeRoute from "./routes/home";
+import streamRoute from "./routes/stream";
 
 const app = express();
 
 app.use(cors());
 app.use("/", homeRoute);
+app.use("/stream", streamRoute);
 
 export default app;
