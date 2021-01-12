71d5c819ad8ef0f1cac5aa28b181efedb9bd810c
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-01-12 14:44:17 +0530

    updating app.ts file with new router api

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
 import express from "express";
 import cors from "cors";
-import homeRoute from "./routes/home";
-import streamRoute from "./routes/stream";
+import morgon from "morgan";
+import router from "./routes/routes";
+import errorHandler from "./utils/errorHandler";
 
 const app = express();
 
+app.use(morgon("dev"));
 app.use(cors());
-app.use("/", homeRoute);
-app.use("/stream", streamRoute);
+app.use(express.json());
+
+app.use("/", router);
+
+// Setting a Error hadler for whole app
+app.use(errorHandler);
 
 export default app;
