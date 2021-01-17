4e565d261dee753276f241d45d4c425b9f55f61c
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-01-17 19:12:24 +0530

    changing stream route

--- src/routes/stream/stream.ts
+++ src/routes/stream/stream.ts
@@ -1,34 +1,19 @@
 import { Router, Request, Response, NextFunction } from "express";
 import mongoose from "mongoose";
+import { promisifyPipeline } from "../../utils/util";
 
-import { tempDir, gridfs } from "../../server";
+import { gridfs } from "../../server";
 
 const router = Router();
 
 router.get("/:id", async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const songID = req.params.id;
-        const id = mongoose.Types.ObjectId(songID);
+        const audioBlobID = req.params.id;
+        const id = mongoose.Types.ObjectId(audioBlobID);
         const downloadStream = gridfs.openDownloadStream(id);
-        const cachedFilename = `${id}.mp3`;
 
-        if (tempDir.getFileList.includes(cachedFilename)) {
-            const data = tempDir.readFileFromTempDir(cachedFilename);
-
-            return res.send(data);
-        } else {
-            const writeFile = tempDir.saveFileInTempDirStream(cachedFilename);
-
-            downloadStream.pipe(writeFile);
-
-            downloadStream.on("end", () => {
-                res.send(tempDir.readFileFromTempDir(cachedFilename));
-            });
-
-            downloadStream.on("error", (err) => {
-                next(err);
-            });
-        }
+        await promisifyPipeline(downloadStream, res);
+        console.log("stream complete");
     } catch (error) {
         next(error);
     }
