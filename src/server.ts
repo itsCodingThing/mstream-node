3b3dac06ff86a12441154f7b81d163c65941c4e8
Bhanu Pratap Singh <bhanuparitapsingh09@gmail.com>
2021-01-19 15:24:41 +0530

    removing temp dir code

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,8 @@
 import "reflect-metadata";
 import app from "./app";
 import { connectToDb } from "./database/db";
-import TempDir from "./utils/tempDir";
 
 const PORT: number | string = process.env.PORT || 1729;
-
-// Setting a temp directory in system temp directory
-export const tempDir = new TempDir("mstream");
 
 export const gridfs = await connectToDb();
 
